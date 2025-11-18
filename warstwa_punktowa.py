f41bdd8cb41b7faccb924477b2da5c56fbf99dd2
ainglot <ainglot@gmail.com>
2025-11-18 12:02:56 +0100

    Budowanie, listy współrzędnych

--- warstwa_punktowa.py
+++ warstwa_punktowa.py
@@ -5,9 +5,13 @@ import arcpy
 arcpy.env.workspace = r"D:\GIS\Rok_2025_26\PPA_ArcGIS\PPA_Gr1.gdb"
 warstwa_punktowa = "GDA2020_OT_OIPR_P"
 
+def odczytywanie_wspolrzednych(warstwa):
+    lista_wsp = []
+    with arcpy.da.SearchCursor(warstwa_punktowa, ['SHAPE@X', 'SHAPE@Y']) as cursor:
+        for row in cursor:
+            lista_wsp.append([row])
+    return lista_wsp
 
-with arcpy.da.SearchCursor(warstwa_punktowa, ['SHAPE@X', 'SHAPE@Y']) as cursor:
-    for row in cursor:
-        print(row)
+print(odczytywanie_wspolrzednych(warstwa_punktowa)[:50])
 
 print("KONIEC")
