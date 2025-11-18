9e218b0a708f366c1179afa26adc7c5f7ee79c6d
ainglot <ainglot@gmail.com>
2025-11-18 12:09:01 +0100

    Zmiana położenia współrzędnych warstwy punktowej.

--- warstwa_punktowa.py
+++ warstwa_punktowa.py
@@ -3,8 +3,9 @@ import arcpy
 
 # === USTAWIENIA ŚRODOWISKA ===
 arcpy.env.workspace = r"D:\GIS\Rok_2025_26\PPA_ArcGIS\PPA_Gr1.gdb"
-warstwa_punktowa = "GDA2020_OT_OIPR_P"
+warstwa_punktowa = "GDA2020_OT_OIPR_P_copy"
 
+# === FUNKCJE DLA WARSTWY PUNKTOWEJ ===
 def odczytywanie_wspolrzednych(warstwa):
     lista_wsp = []
     with arcpy.da.SearchCursor(warstwa_punktowa, ['SHAPE@X', 'SHAPE@Y']) as cursor:
@@ -12,6 +13,15 @@ def odczytywanie_wspolrzednych(warstwa):
             lista_wsp.append([row])
     return lista_wsp
 
-print(odczytywanie_wspolrzednych(warstwa_punktowa)[:50])
+def aktualizowanie_wspolrzednych(warstwa):
+    with arcpy.da.UpdateCursor(warstwa_punktowa, ['SHAPE@X', 'SHAPE@Y']) as cursor:
+        for row in cursor:
+            row[0] += 100
+            row[1] += 100
+            cursor.updateRow(row)
+
+# === WYWOŁYWANIE FUNKCJI ===
+# print(odczytywanie_wspolrzednych(warstwa_punktowa)[:50])
+aktualizowanie_wspolrzednych(warstwa_punktowa)
 
 print("KONIEC")
