2892e208c4949d79f12591d08a6e776032903ac6
ainglot <ainglot@gmail.com>
2025-11-18 11:59:15 +0100

    Odczytywanie współrzędnych warstwy punktowej

--- warstwa_punktowa.py
+++ warstwa_punktowa.py
@@ -3,6 +3,11 @@ import arcpy
 
 # === USTAWIENIA ŚRODOWISKA ===
 arcpy.env.workspace = r"D:\GIS\Rok_2025_26\PPA_ArcGIS\PPA_Gr1.gdb"
+warstwa_punktowa = "GDA2020_OT_OIPR_P"
 
-with arcpy.da.SearchCursor(fc, ['OID@', 'SHAPE@AREA']) as cursor:
 
+with arcpy.da.SearchCursor(warstwa_punktowa, ['SHAPE@X', 'SHAPE@Y']) as cursor:
+    for row in cursor:
+        print(row)
+
+print("KONIEC")
