0caeed3dac50810514c64dc32eeb8af0d245da35
ainglot <ainglot@gmail.com>
2025-12-16 12:57:24 +0100

    Dystans 3d

--- linie3d.py
+++ linie3d.py
@@ -1,6 +1,6 @@
 import arcpy
 import numpy as np
-
+import math
 
 # === USTAWIENIA ŚRODOWISKA ===
 arcpy.env.workspace = r"D:\GIS\Rok_2025_26\PPA_ArcGIS\Geobaza ZTM\ZTM197.gdb"
@@ -32,6 +32,20 @@ def wstawianie_wspolrzednych(warstwa, lista_wsp, field):
             Z = wsp[2]
             cursor.insertRow([X, Y, Z, Z])
 
+def odleglosc_2d(p1, p2):
+    x1, y1, z1 = p1
+    x2, y2, z2 = p2
+    return math.hypot(x2 - x1, y2 - y1)
+
+def odleglosc_3d(p1, p2):
+    x1, y1, z1 = p1
+    x2, y2, z2 = p2
+    return math.sqrt(
+        (x2 - x1)**2 +
+        (y2 - y1)**2 +
+        (z2 - z1)**2
+    )
+
 Wsp_Linie = odczytywanie_wspolrzednych(warstwa_liniowa)
 # print(Wsp_Linie)
 
@@ -49,27 +63,37 @@ PKT = [473593., 721194.]
 PKT = [474432.81, 718876.37] #45.669998
 
 listaWSP = []
-for ras in listR:
-    if punkt_na_rastrze(PKT, ras[1]):
-        R = arcpy.Raster(ras[0])
-        R_array = arcpy.RasterToNumPyArray(R, nodata_to_value = np.nan) 
-        XMIN = ras[1][0]
-        YMAX = ras[1][3]
-        CellSIZE = R.meanCellWidth
+for linia in Wsp_Linie:
+    for PKT in linia:
+        for ras in listR:
+            if punkt_na_rastrze(PKT, ras[1]):
+                R = arcpy.Raster(ras[0])
+                R_array = arcpy.RasterToNumPyArray(R, nodata_to_value = np.nan) 
+                XMIN = ras[1][0]
+                YMAX = ras[1][3]
+                CellSIZE = R.meanCellWidth
 
-        dx = (PKT[0] - XMIN) * CellSIZE
-        dy = (YMAX - PKT[1]) * CellSIZE
+                dx = (PKT[0] - XMIN) * CellSIZE
+                dy = (YMAX - PKT[1]) * CellSIZE
 
-        row = int(dy)
-        col = int(dx)
-        print(dx, dy, row, col, R_array[row, col])
-        if not np.isnan(R_array[row, col]):
-            listaWSP.append([PKT[0], PKT[1], R_array[row, col]])
+                row = int(dy)
+                col = int(dx)
+                print(dx, dy, row, col, R_array[row, col])
+                if not np.isnan(R_array[row, col]):
+                    listaWSP.append([PKT[0], PKT[1], R_array[row, col]])
 
-nowa_warstwa = "PunktyNaRastrze02"
-arcpy.env.workspace = r"D:\GIS\Rok_2025_26\PPA_ArcGIS\Geobaza ZTM\ZTM197.gdb"
-arcpy.management.CreateFeatureclass(arcpy.env.workspace, nowa_warstwa, "POINT", "", "DISABLED", "ENABLED", warstwa_liniowa)
-arcpy.management.AddField(nowa_warstwa, "wsp_z", "FLOAT")
-wstawianie_wspolrzednych(nowa_warstwa, listaWSP, "wsp_z")
+nowa_warstwa = "PunktyNaRastrze03"
+# arcpy.env.workspace = r"D:\GIS\Rok_2025_26\PPA_ArcGIS\Geobaza ZTM\ZTM197.gdb"
+# arcpy.management.CreateFeatureclass(arcpy.env.workspace, nowa_warstwa, "POINT", "", "DISABLED", "ENABLED", warstwa_liniowa)
+# arcpy.management.AddField(nowa_warstwa, "wsp_z", "FLOAT")
+# wstawianie_wspolrzednych(nowa_warstwa, listaWSP, "wsp_z")
+
+s2d = 0.
+s3d = 0.
+for i in range(len(listaWSP)-1):
+    xyz0 = listaWSP[i]
+    xyz1 = listaWSP[i+1]
+    s2d += odleglosc_2d(xyz0, xyz1)
+    s3d += odleglosc_3d(xyz0, xyz1)
 
 print("KONIEC")
