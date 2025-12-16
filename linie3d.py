79188eb61563a2f6f5714ecd980290448bde2aba
ainglot <ainglot@gmail.com>
2025-12-16 12:18:19 +0100

    Zakres współrzędnych rastra.

--- linie3d.py
+++ linie3d.py
@@ -23,7 +23,12 @@ Wsp_Linie = odczytywanie_wspolrzednych(warstwa_liniowa)
 
 arcpy.env.workspace = r"D:\GIS\Rok_2025_26\PPA_ArcGIS\NMT pod ZTM\ZTM197_NMT_TIF"
 rasters = arcpy.ListRasters("*", "TIF")
+
+listR = []
 for raster in rasters:
     print(raster)
+    R = arcpy.Raster(raster)
+    listR.append([raster, [R.extent.XMin, R.extent.YMin, R.extent.XMiax, R.extent.YMax]])
+print(listR)
 
 print("KONIEC")
